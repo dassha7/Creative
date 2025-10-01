08a2c69b6febc4ec69c918255ba986573e5a10bb
dassha7 <daril@pss1.ukr.education>
2025-10-01 11:08:26 +0300

    Add project

---
+++ creative/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { StartCreativity } from './start-creativity/start-creativity';
+
+export const routes: Routes = [
+    {path:"start", component: StartCreativity}
+];
---
+++ creative/src/app/start-creativity/start-creativity.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-start-creativity',
+  imports: [],
+  templateUrl: './start-creativity.html',
+  styleUrl: './start-creativity.css'
+})
+export class StartCreativity {
+
+}
