64b562279f199db60e09433192426f6b397e111a
kkkk222221 <katyazalenko@gmail.com>
2025-10-13 08:48:46 +0300

    Add creativity type

--- creative/src/app/app.routes.ts
+++ creative/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { StartCreativity } from './start-creativity/start-creativity';
+import { CreativityType } from './creativity-type/creativity-type';
 
 export const routes: Routes = [
-    {path:"start", component: StartCreativity}
+    {path:"start", component: StartCreativity},
+    {path:"type", component: CreativityType}
 ];
---
+++ creative/src/app/creativity-type/creativity-type.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-creativity-type',
+  imports: [],
+  templateUrl: './creativity-type.html',
+  styleUrl: './creativity-type.css'
+})
+export class CreativityType {
+
+}
