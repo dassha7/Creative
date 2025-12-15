43bb4f329a8c969c13ec44b1d3254ac0cec6ada4
dassha7 <daril@pss1.ukr.education>
2025-12-15 09:07:40 +0200

    змінила кнопку

--- creative/src/app/start-creativity/start-creativity.ts
+++ creative/src/app/start-creativity/start-creativity.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-start-creativity',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './start-creativity.html',
   styleUrl: './start-creativity.css'
 })
